df2d18a282dca5bdebdb2135c8cb44a776569e36
Tejpal Singh <dev.tezoo@gmail.com>
2025-09-08 18:55:05 +0530

    logo changed

--- src/pages/Page7.jsx
+++ src/pages/Page7.jsx
@@ -4,9 +4,12 @@ const Page7 = () => {
   return (
     <div className='h-[50vh] p-5'>
         <div className='bg-black h-[90%] w-[90%] rounded-4xl relative p-10 text-white'>
+          <div className="footer-moving-txt absolute h-[24vh] w-[20vh]">
+            <img id='foot-anime' className='h-[20vh] w-[20vh] rounded-full' src="/public/footer-img.png" alt="" />
+          </div>
             <div className='absolute bottom-[35px]'> <i class="ri-map-pin-line"></i> &nbsp; Jodhpur, Rajasthan, India</div>
 
-            <div className='absolute left-[37vw]'> <i className="ri-mail-fill"></i> &nbsp; dev.tezoo@gmail.com</div>
+            <div className='absolute left-[37vw] text-[2rem]'> <i className="ri-mail-fill"></i> &nbsp; dev.tezoo@gmail.com</div>
 
             <div className='absolute right-[35px] bottom-[35px]'>&copy; 2025 Tejpal Singh</div>
 
