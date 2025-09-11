89d50e084a33ce71bbecaa4eaf3b071fcf920d36
Tejpal Singh <dev.tezoo@gmail.com>
2025-09-11 19:22:24 +0530

    page3 done

--- src/pages/Page3.jsx
+++ src/pages/Page3.jsx
@@ -3,16 +3,16 @@ import React from 'react'
 const Page3 = () => {
   return (
     <div id='page3' className='text-white h-screen py-[2vh] px-[1vw]'>
-        <div className='bg-black h-[96vh] rounded-4xl px-10'>
-              <h1 className='text-[18rem] font-[font1] uppercase'>About</h1>
-              <h2 className='text-2xl font-[font2]'>"I design the web’s stage where ideas perform and users applaud."</h2><br />
-              <h3 className='w-[70vw] font-[font2]'>
-I’m a frontend developer specializing in React, Tailwind CSS, and Three.js, creating responsive, interactive, and visually striking web applications. My focus is on delivering smooth user experiences, clean code, and performance-optimized designs.
+      <div className='bg-black h-[96vh] rounded-4xl px-10'>
+        <h1 className='about text-[18rem] font-[font1] uppercase'>About</h1>
+        <h2 className='text-2xl font-[font2]'>"I design the web’s stage where ideas perform and users applaud."</h2><br />
+        <h3 className='pg3-h3 w-[70vw] font-[font2]'>
+          I’m a frontend developer specializing in React, Tailwind CSS, and Three.js, creating responsive, interactive, and visually striking web applications. My focus is on delivering smooth user experiences, clean code, and performance-optimized designs.
 
-While my current expertise lies in the frontend, I’m consistently expanding my skill set to grow as a full-stack developer. Right now, I’m diving deep into backend development with Flask and SQLAlchemy, learning how to design and integrate efficient server-side systems with my frontend work.
+          While my current expertise lies in the frontend, I’m consistently expanding my skill set to grow as a full-stack developer. Right now, I’m diving deep into backend development with Flask and SQLAlchemy, learning how to design and integrate efficient server-side systems with my frontend work.
 
-Every project is a chance to level up my craft — to mix creativity with technical precision — and I’m here to build things that make people stop, click, and smile.</h3>
-        </div>
+          Every project is a chance to level up my craft — to mix creativity with technical precision — and I’m here to build things that make people stop, click, and smile.</h3>
+      </div>
     </div>
   )
 }
