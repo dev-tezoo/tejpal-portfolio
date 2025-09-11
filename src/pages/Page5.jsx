6222f0a6b73ea84b9c0b6a9a9614146b6cc25bc8
Tejpal Singh <dev.tezoo@gmail.com>
2025-09-11 20:01:54 +0530

    page5 done

--- src/pages/Page5.jsx
+++ src/pages/Page5.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const Page5 = () => {
     return (
         <div className='h-[120vh] p-[2vh]'>
-            <div className='bg-black text-white h-[116vh] rounded-4xl p-10 '>
+            <div className='pg5-kala-box bg-black text-white
+             h-[116vh] rounded-4xl p-10 '>
 
 
                 <div id='pg5-left' className=' text-[8rem] font-[font1] uppercase'>Services</div>
@@ -14,16 +15,16 @@ const Page5 = () => {
                     <div id='1' className='flex gap-5 items-center'>
                         <div className='w-[40px] h-[40px] flex items-center justify-center rounded bg-gray-900'>1</div>
                         <div>
-                            <h3 className='text-[1.6rem]'>UX Design</h3>
-                            <p className='text-gray-300 text-[1rem]'>Restructuring user flows and creating new wireframes, we can work together to create a seamless and enjoyable experience for your users.</p>
+                            <h3 className='h3 text-[1.6rem]'>UX Design</h3>
+                            <p className='p text-gray-300 text-[1rem]'>Restructuring user flows and creating new wireframes, we can work together to create a seamless and enjoyable experience for your users.</p>
                         </div>
                     </div>
 
                     <div id='2' className='flex gap-5 items-center'>
                         <div className='w-[40px] h-[40px] flex items-center justify-center rounded bg-gray-900'>2</div>
                         <div>
-                            <h3 className='text-[1.6rem]'>UI Design</h3>
-                            <p className='text-gray-300 text-[1rem]'>User Interface (UI) help create a visually stunning and engaging experience for your users, resulting in greater appreciation for your product or services.</p>
+                            <h3 className='h3 text-[1.6rem]'>UI Design</h3>
+                            <p className='p text-gray-300 text-[1rem]'>User Interface (UI) help create a visually stunning and engaging experience for your users, resulting in greater appreciation for your product or services.</p>
                         </div>
                     </div>
 
@@ -31,8 +32,8 @@ const Page5 = () => {
                     <div id='3' className='flex gap-5 items-center'>
                         <div className='w-[40px] h-[40px] flex items-center justify-center rounded bg-gray-900'>3</div>
                         <div>
-                            <h3 className='text-[1.6rem]'>Web Design</h3>
-                            <p className='text-gray-300 text-[1rem]'>Web Design can be considered a powerful marketing tool. I specialize in designing websites that effectively generates leads for your company, helping to drive growth and success.</p>
+                            <h3 className='h3 text-[1.6rem]'>Web Design</h3>
+                            <p className='p text-gray-300 text-[1rem]'>Web Design can be considered a powerful marketing tool. I specialize in designing websites that effectively generates leads for your company, helping to drive growth and success.</p>
                         </div>
                     </div>
 
@@ -40,8 +41,8 @@ const Page5 = () => {
                     <div id='4' className='flex gap-5 items-center'>
                         <div className='w-[40px] h-[40px] flex items-center justify-center rounded bg-gray-900'>4</div>
                         <div>
-                            <h3 className='text-[1.6rem]'>Frontend Development</h3>
-                            <p className='text-gray-300 text-[1rem]'>I specialize in creating custom web designs and then bringing them to life using HTML, CSS & JavaScript (React/Typescript)</p>
+                            <h3 className='h3 text-[1.6rem]'>Frontend Development</h3>
+                            <p className='p text-gray-300 text-[1rem]'>I specialize in creating custom web designs and then bringing them to life using HTML, CSS & JavaScript (React/Typescript)</p>
                         </div>
                     </div>
 
