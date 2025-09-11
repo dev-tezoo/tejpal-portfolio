5663a73a40c30c355dd872e000f4a7dd12d1e637
Tejpal Singh <dev.tezoo@gmail.com>
2025-09-11 21:24:15 +0530

    rer

--- src/components/TiltText.jsx
+++ src/components/TiltText.jsx
@@ -4,8 +4,8 @@ const TiltText = () => {
   return (
     <div className='tilt-text flex items-center flex-col justify-start my-25'>
         <h1 className='tilt-txt-l1 leading-[4vw] text-[6.5vw] uppercase font-[font2]'>I am a <span className='text-black'>Frontend</span></h1>
-        <h2 className='tilt-txt-l2 text-[8vw] leading-[8vw] uppercase font-[font2]'>Web Designer</h2>
-        <h3 className='tilt-txt-l3 text-[4.5vw] leading-[5vw] uppercase font-[font2]'>& Developer</h3>
+        <h2 className='tilt-txt-l2 text-[8vw] leading-[8vw] uppercase font-[font2]'>Web Developer</h2>
+        <h3 className='tilt-txt-l3 text-[4.5vw] leading-[5vw] uppercase font-[font2]'>& Designer</h3>
     </div>
   )
 }
