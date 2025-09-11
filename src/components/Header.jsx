e9c8c5132a2b805431e1862a6cdded4eeb7fc758
Tejpal Singh <dev.tezoo@gmail.com>
2025-09-11 18:50:45 +0530

    page 1 media

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,33 +5,33 @@ import 'remixicon/fonts/remixicon.css'
 
 const Header = () => {
 
-useEffect(() => {
-  const menu = document.getElementById('menu-icon');
-  const banner = document.getElementById('menu-banner');
-  menu.addEventListener('click', () => {
-    banner.classList.toggle('opacity-100');
-    setTimeout(() => {
-      banner.classList.toggle('opacity-0');
-    }, 300);
-  });
+  useEffect(() => {
+    const menu = document.getElementById('menu-icon');
+    const banner = document.getElementById('menu-banner');
+    menu.addEventListener('click', () => {
+      banner.classList.toggle('opacity-100');
+      setTimeout(() => {
+        banner.classList.toggle('opacity-0');
+      }, 300);
+    });
 
-  return () => {
-    if (menu) {
+    return () => {
+      if (menu) {
         menu.replaceWith(menu.cloneNode(true));
       }
-  }
-}, [])
+    }
+  }, [])
 
 
 
 
   return (
-    <div className=' fixed flex items-center justify-end w-full z-10 py-20 px-20'>
- <a href="/assets/TPCV.pdf" download={"TejpalCV.pdf"}>     <button id='top-btn' className='bg-[#02090F] px-4 py-2 hover:bg-white hover:scale-110 mr-2 transition-all duration-300 hover:text-black  rounded-full text-white text-xl '>Resume <i className="ri-download-fill"></i></button></a>
+    <div className='pg1-top-right fixed flex items-center justify-end w-full z-10 py-20 px-20'>
+      <a href="/assets/TPCV.pdf" download={"TejpalCV.pdf"}>     <button id='top-btn' className='bg-[#02090F] px-4 py-2 hover:bg-white hover:scale-110 mr-2 transition-all duration-300 hover:text-black  rounded-full text-white text-xl '>Resume <i className="ri-download-fill"></i></button></a>
 
       <i id='menu-icon' className="ri-menu-line text-4xl ml-3 hover:scale-120 transition-all duration-300 text-gray-500 hover:transform hover:rotate-90"></i>
 
-      <div id='menu-banner' className='menu-banner opacity-0 h-[15vh] flex items-center text-center justify-center bg-amber-700 w-[17vw] absolute -bottom-10 right-15 rounded-2xl font-bold font-[font2] text-2xl'>You lazy Clicker! <br /> Just Scroll <i className="ri-home-fill absolute text-2xl text-amber-700 h-[20px] w-[20px] -top-6 right-8"></i></div>
+      <div id='menu-banner' className='menu-banner opacity-0 h-[15vh] flex items-center text-center justify-center bg-amber-700 w-[17vw] absolute -bottom-10 right-15 rounded-2xl font-bold font-[font2] text-2xl'>You lazy Clicker! <br /> Just Scroll <i className="tunn ri-home-fill absolute text-2xl text-amber-700 h-[20px] w-[20px] -top-6 right-8"></i></div>
 
     </div>
   )
--- src/components/Page1Bottom.jsx
+++ src/components/Page1Bottom.jsx
@@ -1,11 +1,11 @@
 import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
-import React, {useEffect} from 'react'
+import React, { useEffect } from 'react'
 
 const Page1Bottom = () => {
 
-  useGSAP(function(){
-    gsap.to('#btm-git',{
+  useGSAP(function () {
+    gsap.to('#btm-git', {
       rotate: 360,
       duration: 2,
       repeat: -1,
@@ -13,8 +13,8 @@ const Page1Bottom = () => {
     })
   })
 
-  useGSAP(function(){
-    gsap.to('#btm-linkedin',{
+  useGSAP(function () {
+    gsap.to('#btm-linkedin', {
       rotate: 360,
       duration: 2,
       repeat: -1,
@@ -22,8 +22,8 @@ const Page1Bottom = () => {
     })
   })
 
-  useGSAP(function(){
-    gsap.to('#btm-instagram',{
+  useGSAP(function () {
+    gsap.to('#btm-instagram', {
       rotate: 360,
       duration: 2,
       repeat: -1,
@@ -31,8 +31,8 @@ const Page1Bottom = () => {
     })
   })
 
-  useGSAP(function(){
-    gsap.to('#btm-twitter',{
+  useGSAP(function () {
+    gsap.to('#btm-twitter', {
       rotate: 360,
       duration: 2,
       repeat: -1,
@@ -62,35 +62,35 @@ const Page1Bottom = () => {
 
   return (
     <div className='pg1-btm absolute flex items-center justify-between bottom-10 mx-[90px] my-[20px] w-[90vw] h-[9vh] left-0'>
-        <div className='pg1-btm-txt text-2xl font-bold'>
-            <h1>Website Designer | Freelancing | Creative UI</h1>
-        </div>
-        <div id='btm-git' className='text-5xl pg-3clr right-18 fixed text-gray-800'>
-            <a href="https://github.com/dev-tezoo">
-                <i className="ri-github-fill"></i>
-            </a>
-        </div>
+      <div className='pg1-btm-txt text-2xl font-bold'>
+        <h1>Website Designer | Freelancing | Creative UI</h1>
+      </div>
+      <div id='btm-git' className='text-5xl pg-3clr right-18 fixed text-gray-800'>
+        <a href="https://github.com/dev-tezoo">
+          <i className="ri-github-fill"></i>
+        </a>
+      </div>
 
 
-        <div id='btm-linkedin' className='text-5xl pg-3clr right-18 bottom-35 fixed text-gray-800'>
-            <a href="https://www.linkedin.com/in/tejpalsingh26/">
-                <i className="ri-linkedin-box-fill"></i>
-            </a>
-        </div>
+      <div id='btm-linkedin' className='text-5xl pg-3clr right-18 bottom-35 fixed text-gray-800'>
+        <a href="https://www.linkedin.com/in/tejpalsingh26/">
+          <i className="ri-linkedin-box-fill"></i>
+        </a>
+      </div>
 
-        <div id='btm-instagram' className='text-5xl pg-3clr right-18 bottom-55 fixed text-gray-800'>
-            <a href="https://www.instagram.com/tezzooooooo/">
-                <i className="ri-instagram-fill"></i>
-            </a>
-        </div>
+      <div id='btm-instagram' className='text-5xl pg-3clr right-18 bottom-55 fixed text-gray-800'>
+        <a href="https://www.instagram.com/tezzooooooo/">
+          <i className="ri-instagram-fill"></i>
+        </a>
+      </div>
 
-        <div id='btm-twitter' className='text-5xl pg-3clr right-18 bottom-75 fixed text-gray-800'>
-            <a href="https://x.com/TezooRathore">
-                <i className="ri-twitter-x-fill"></i>
-            </a>
-        </div>
+      <div id='btm-twitter' className='text-5xl pg-3clr right-18 bottom-75 fixed text-gray-800'>
+        <a href="https://x.com/TezooRathore">
+          <i className="ri-twitter-x-fill"></i>
+        </a>
+      </div>
 
-        
+
     </div>
   )
 }
