5e1151550c85baad297153b80aacd33f9f431669
Tejpal Singh <dev.tezoo@gmail.com>
2025-09-12 11:36:52 +0530

    iii

--- src/pages/Page7.jsx
+++ src/pages/Page7.jsx
@@ -4,12 +4,10 @@ const Page7 = () => {
   return (
     <div className='h-[50vh] p-5'>
       <div id='footer' className='bg-black h-[90%] w-[90%] rounded-4xl relative p-10 text-white'>
-        <div id='gg' className="footer-moving-txt absolute h-[24vh] w-[20vh]">
+        <div id='gg' className="footer-moving-txt absolute h-[24vh] w-[20vh] bg-amber-300">
           <img id='foot-anime' className='h-[20vh] w-[20vh] rounded-full' src="/src/assets/footer-img2.png" alt="" />
         </div>
-        <div className="footer-moving-txt absolute h-[24vh] w-[20vh]">
-          <img id='foot-anime' className='h-[20vh] w-[20vh] rounded-full' src="/src/assets/footer-img2.png" alt="" />
-        </div>
+        
         <div className='add-txt absolute bottom-[35px]'> <i class="ri-map-pin-line"></i> &nbsp; Jodhpur, Rajasthan, India</div>
 
         <div className='mail-txt absolute left-[37vw] text-[2rem]'> <i className="ri-mail-fill"></i> &nbsp; dev.tezoo@gmail.com</div>
